50b47c6c118f0f855088be13aa8487b21f3ffd05
Raphael Sander <raphael.sander75@gmail.com>
2026-01-05 17:46:58 -0300

    refactor(media): migrate proxmox lxc resource
    closes #5

--- media/main.tf
+++ media/main.tf
@@ -9,61 +9,69 @@ terraform {
 
 provider "proxmox" {
   pm_api_url = "https://<domain>/api2/json"
-  pm_debug = true
+  pm_debug   = true
   # pm_tls_insecure = true
 }
 
-resource "proxmox_lxc" "media" {
-    hostname     = var.hostname
-    unprivileged = false
-    target_node  = "proxmox-1"
-    password     = var.root_password
-    ostemplate   = "local:vztmpl/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
-    onboot       = true
-    start        = true
-    tags         = "docker;ubuntu"
+resource "proxmox_lxc_guest" "media" {
+  name        = var.hostname
+  privileged  = true
+  target_node = "proxmox-1"
+  password    = var.root_password
+  template {
+    file    = "ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
+    storage = "local"
+  }
+  tags = ["docker", "ubuntu"]
+  cpu {
     cores = 4
-    memory = 4096
-    swap   = 512
-    ssh_public_keys = file("~/.ssh/id_rsa.pub")
-    features {
-        nesting = true
+  }
+  memory = 4096
+  swap   = 512
+  startup_shutdown {}
+  ssh_public_keys = file("~/.ssh/id_rsa.pub")
+  features {
+    privileged {
+      nesting = true
     }
-    rootfs {
-        size    = "8G"
-        storage = "local-lvm"
-    }
-    mountpoint {
-        key     = "0"
-        slot    = 0
-        storage = "zfs-tank"
-        mp      = "/mnt/media"
-        size    = "300G"
-        backup  = false
-    }
-    network {
-        name = "eth0"
-        bridge = "vmbr1"
-        ip = "172.16.1.30/30"
-        gw = "172.16.1.29"
-        tag = 4
-    }
+  }
+  root_mount {
+    size    = "8G"
+    storage = "local-lvm"
+  }
+  mount {
+    slot       = "mp0"
+    storage    = "zfs-tank"
+    guest_path = "/mnt/media"
+    size       = "300G"
+    backup     = false
+    replicate  = false
+    type       = "data"
+  }
+  network {
+    id           = 0
+    name         = "eth0"
+    bridge       = "vmbr1"
+    ipv4_address = "172.16.1.30/30"
+    ipv4_gateway = "172.16.1.29"
+    vlan_native  = 4
+  }
 }
 
 resource "null_resource" "ansible" {
-    triggers = {
-        lxc_id = proxmox_lxc.media.id
-        playbook_hash = filesha1("playbook.yml")
-    }
+  triggers = {
+    lxc_id        = proxmox_lxc_guest.media.id
+    playbook_hash = filesha1("playbook.yml")
+  }
 
-    provisioner "local-exec" {
-        command = <<-EOF
-            ANSIBLE_HOST_KEY_CHECKING=False \
-            ansible-playbook \
-                -i ${split("/", proxmox_lxc.media.network[0].ip)[0]}, \
-                --user root \
-                --timeout 10 \
-                playbook.yml
-        EOF
-    }
+  provisioner "local-exec" {
+    command = <<-EOF
+      ANSIBLE_HOST_KEY_CHECKING=False \
+      ansible-playbook \
+        -i ${split("/", proxmox_lxc_guest.media.network[0].ipv4_address)[0]}, \
+        --user root \
+        --timeout 10 \
+        playbook.yml
+    EOF
+  }
 }
